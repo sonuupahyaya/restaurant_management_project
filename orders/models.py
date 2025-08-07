4b4a3c5393b9dca25f63807c269497afd165ee63
sonuupahyaya <98040472+sonuupahyaya@users.noreply.github.com>
2025-08-07 15:35:36 +0530

    24248

--- orders/models.py
+++ orders/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from account.models import Customer
+from products.models import Product
 
-# Create your models here.
+class Order(models.Model):
+    customer = models.ForeignKey(Customer, on_delete=models.CASCADE)
+        product = models.ForeignKey(Product, on_delete=models.CASCADE)
+            quantity = models.PositiveIntegerField()
+                date = models.DateTimeField(auto_now_add=True)
+                
