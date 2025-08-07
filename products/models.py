403a3c2f56ec9de02c21b2a41acaba5d9640098e
sonuupahyaya <98040472+sonuupahyaya@users.noreply.github.com>
2025-08-07 15:37:12 +0530

    24248

--- products/models.py
+++ products/models.py
@@ -1,10 +1,7 @@
 from django.db import models
 
-# Create your models here.
-class Item(models.Model):
-    item_name = models.CharField(max_length=150)
-    item_price = models.DecimalField(max_digits=10, decimal_places=2)
-    created_at = models.DateTimeField(auto_now_add=True)
-
-    def __str__(self):
-        return str(self.item_name)
+class Product(models.Model):
+    name = models.CharField(max_length=100)
+        price = models.DecimalField(max_digits=8, decimal_places=2)
+            available = models.BooleanField(default=True)
+            
