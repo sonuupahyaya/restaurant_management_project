f8637722fbd385bd58d398810156610cace3d92b
sonuupahyaya <98040472+sonuupahyaya@users.noreply.github.com>
2025-08-07 15:33:41 +0530

    24248

--- account/models.py
+++ account/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
-# Create your models here.
+class Customer(models.Model):
+    name = models.CharField(max_length=100)
+        email = models.EmailField(unique=True)
+            phone = models.CharField(max_length=15)
+            
