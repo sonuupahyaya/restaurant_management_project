dd792387c891fe743bc3f3b0ec8da21a776c8467
sonuupahyaya <98040472+sonuupahyaya@users.noreply.github.com>
2025-08-07 15:34:23 +0530

    24248

--- account/views.py
+++ account/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from .models import Customer
 
-# Create your views here.
+def customer_list(request):
+    customers = Customer.objects.all()
+        return render(request, 'account/customers.html', {'customers': customers})
+            
