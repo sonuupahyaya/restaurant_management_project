c2778bb2a768a80d14f2be4c40ea18eee70ee43d
sonuupahyaya <98040472+sonuupahyaya@users.noreply.github.com>
2025-08-08 21:26:30 +0530

    24251

--- account/views.py
+++ account/views.py
@@ -5,3 +5,16 @@ def customer_list(request):
     customers = Customer.objects.all()
         return render(request, 'account/customers.html', {'customers': customers})
             
+            from django.shortcuts import render
+            from django.conf import settings  # to access settings.py variables
+
+            def home(request):
+                """
+                    View to display the homepage with restaurant name fetched from settings.py
+                        """
+                            restaurant_name = getattr(settings, 'RESTAURANT_NAME', 'Our Restaurant')  # default if not found
+                                
+                                    return render(request, 'home.html', {
+                                            'restaurant_name': restaurant_name
+                                                })
+                                                
