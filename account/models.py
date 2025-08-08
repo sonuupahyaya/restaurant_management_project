2467d9a6dffe4c43c73442efb64a49ce0e0d2d4f
sonuupahyaya <98040472+sonuupahyaya@users.noreply.github.com>
2025-08-08 21:29:13 +0530

    24252

--- account/models.py
+++ account/models.py
@@ -5,3 +5,14 @@ class Customer(models.Model):
         email = models.EmailField(unique=True)
             phone = models.CharField(max_length=15)
             
+            from django.db import models
+
+            # Restaurant model to store basic info
+            class Restaurant(models.Model):
+                name = models.CharField(max_length=200)
+                    description = models.TextField()
+                        image = models.ImageField(upload_to='restaurant_images/', blank=True, null=True)
+
+                            def __str__(self):
+                                    return self.name
+                                    
