e3cefdc9605c437db218eb3987fb96d486b2a183
sonuupahyaya <98040472+sonuupahyaya@users.noreply.github.com>
2025-08-08 21:23:38 +0530

    24251

--- restaurant_management/settings.py
+++ restaurant_management/settings.py
@@ -127,3 +127,11 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+# settings.py
+
+# ... your existing settings
+
+# Add your restaurant name here
+RESTAURANT_NAME = "TableTrek Delight"
