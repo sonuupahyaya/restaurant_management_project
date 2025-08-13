13e20e06013fe28015f1efbcd72e5e4eb6fd7d64
sonuupahyaya <98040472+sonuupahyaya@users.noreply.github.com>
2025-08-13 12:23:50 +0530

    31326

--- products/views.py
+++ products/views.py
@@ -1,7 +1,40 @@
-from django.shortcuts import render
-from .models import Product
+# products/views.py
 
-def product_list(request):
-    products = Product.objects.all()
-        return render(request, 'products/products.html', {'products': products})
-        
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+
+class MenuListView(APIView):
+    def get(self, request):
+            """
+            Returns a list of menu items.
+            Currently uses a hardcoded list, but later this
+            will be replaced with a database query.
+            """
+            menu_items = [
+                {
+                    "id": 1,
+                    "name": "Margherita Pizza",
+                    "price": 8.99,
+                    "category": "Pizza"
+                },
+                {
+                   "id": 2,
+                   "name": "Pepperoni Pizza",
+                   "price": 10.99,
+                   "category": "Pizza"
+                },
+                {
+                   "id": 3,
+                   "name": "Caesar Salad",
+                   "price": 6.50,
+                   "category": "Salad"
+                },
+                {
+                   "id": 4,
+                   "name": "Spaghetti Bolognese",
+                   "price": 12.00,
+                   "category": "Pasta"
+                }
+            ]
+ return Response(menu_items, status=status.HTTP_200_OK)
