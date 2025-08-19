9b0c907e6bceacc2c765f7797389250e727cc930
sonuupahyaya <98040472+sonuupahyaya@users.noreply.github.com>
2025-08-19 21:05:40 +0530

    34652

--- account/admin.py
+++ account/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from .models import Menu, Order
 
-# Register your models here.
+@admin.register(Menu)
+class MenuAdmin(admin.ModelAdmin):
+    list_display = ("id", "name", "price")  # show these columns in admin list
+        search_fields = ("name",)               # allow searching by name
+            list_filter = ("price",)                # allow filtering by price
+
+
+            @admin.register(Order)
+            class OrderAdmin(admin.ModelAdmin):
+                list_display = ("id", "customer_name", "status", "total_amount", "created_at")
+                    search_fields = ("customer_name", "status")
+                        list_filter = ("status", "created_at")
+                        
