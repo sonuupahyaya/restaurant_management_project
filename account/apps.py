806a65d6fe77cd2cdd89a623696dbb2f9ad39b68
sonuupahyaya <98040472+sonuupahyaya@users.noreply.github.com>
2025-08-19 21:32:48 +0530

    34649

--- account/apps.py
+++ account/apps.py
@@ -3,4 +3,10 @@ from django.apps import AppConfig
 class AccountConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
         name = 'account'
-        
+        class RestaurantConfig(AppConfig):
+                default_auto_field = 'django.db.models.BigAutoField'
+                    name = 'restaurant'  # change to your app name
+
+                        def ready(self):
+                                import restaurant.signals  # import signals
+                                
