53f136435ae73961e08e46eb947812cbdadd2bf4
sonuupahyaya <98040472+sonuupahyaya@users.noreply.github.com>
2025-08-22 21:35:19 +0530

    37314

--- account/models.py
+++ account/models.py
@@ -91,4 +91,11 @@ class Menu(models.Model):
 
                         def __str__(self):
                                 return f"{self.address}, {self.city}, {self.state} {self.zip_code}"
-                                
+
+class Contact(models.Model):                                
+        name = models.CharField(max_length=100)
+            email = models.EmailField()
+                submitted_at = models.DateTimeField(auto_now_add=True)
+
+                    def __str__(self):
+                            return f"{self.name} - {self.email}"
