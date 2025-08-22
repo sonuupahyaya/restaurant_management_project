2aac343f81c23bea221ff56fc48e2bd56022bd69
sonuupahyaya <98040472+sonuupahyaya@users.noreply.github.com>
2025-08-22 21:41:09 +0530

    37314

--- home/urls.py
+++ home/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
 from . import views
+from .view import contact_View
 
 urlpatterns = [
     path("contact/", views.contact_us, name="contact_us"),
@@ -15,4 +16,7 @@ urlpatterns = [
             urlpatterns = []
                 path("", views.home, name="home"),
                 ]
-                
+ urlpatterns = []               
+     path("contact/", contact_view, name="contact"),
+     ]
+     
