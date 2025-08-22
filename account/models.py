e6f624d00a101ea95f23eb918963cce290327e67
sonuupahyaya <98040472+sonuupahyaya@users.noreply.github.com>
2025-08-22 21:31:50 +0530

    37315

--- account/models.py
+++ account/models.py
@@ -83,4 +83,12 @@ class Menu(models.Model):
 
                     def __str__(self):
                             return f"{self.name} - ${self.price}"
-                            
+ class RestaurantLocation(models.Model):                           
+        address = models.CharField(max_length=255)
+            city = models.CharField(max_length=100)
+                state = models.CharField(max_length=100)
+                    zip_code = models.CharField(max_length=10)
+
+                        def __str__(self):
+                                return f"{self.address}, {self.city}, {self.state} {self.zip_code}"
+                                
