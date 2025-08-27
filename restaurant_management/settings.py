68f6f3a3eaf46ee4494178b16f082087925b9ac1
sonuupahyaya <98040472+sonuupahyaya@users.noreply.github.com>
2025-08-27 18:10:25 +0530

    39190

--- restaurant_management/settings.py
+++ restaurant_management/settings.py
@@ -146,3 +146,5 @@ MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
