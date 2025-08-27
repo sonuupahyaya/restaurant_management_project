b2429a21123db9a7f24a75bbc5b2cbd5d469222e
sonuupahyaya <98040472+sonuupahyaya@users.noreply.github.com>
2025-08-27 18:17:38 +0530

    39189

--- forms.py
+++ forms.py
@@ -6,3 +6,8 @@ class ContactForm(forms.ModelForm):
             model = Contact
                     fields = ['name', 'email']
                     
+                    class ContactForm(forms.Form):
+                        name = forms.CharField(max_length=100)
+                            email = forms.EmailField()
+                                message = forms.CharField(widget=forms.Textarea)
+                                
