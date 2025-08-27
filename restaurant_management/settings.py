c72aa93e2a8fbdff2300634db863c4ec3a004d77
sonuupahyaya <98040472+sonuupahyaya@users.noreply.github.com>
2025-08-27 18:17:12 +0530

    39189

--- restaurant_management/settings.py
+++ restaurant_management/settings.py
@@ -148,3 +148,11 @@ MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+# Email settings
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'  # Dev only
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = 'yourrestaurant@gmail.com'  # Replace later
+EMAIL_HOST_PASSWORD = 'your-app-password'     # Replace later
+DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
