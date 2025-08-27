9ea02d582ea7a4e5639f2b48c1eb4cfe4d795bc0
sonuupahyaya <98040472+sonuupahyaya@users.noreply.github.com>
2025-08-27 18:09:56 +0530

    39190

--- account/models.py
+++ account/models.py
@@ -131,3 +131,11 @@ class Menu(models.Model):
 
                                                 def __str__(self):
                                                         return self.name
+class MenuItem(models.Model):                                                        
+        name = models.CharField(max_length=100)
+            description = models.TextField(blank=True, null=True)
+                price = models.DecimalField(max_digits=6, decimal_places=2)
+                    image = models.ImageField(upload_to='menu_images/', blank=True, null=True)  # NEW FIELD
+
+                        def __str__(self):
+                                return self.name
