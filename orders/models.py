5213750f885bf4eae7efedd90121563e52e1ee6c
sonuupahyaya <98040472+sonuupahyaya@users.noreply.github.com>
2025-09-03 21:01:10 +0530

    42604

--- orders/models.py
+++ orders/models.py
@@ -4,7 +4,7 @@ from products.models import Product
 
 class Order(models.Model):
     customer = models.ForeignKey(Customer, on_delete=models.CASCADE)
-        product = models.ForeignKey(Product, on_delete=models.CASCADE)
-            quantity = models.PositiveIntegerField()
-                date = models.DateTimeField(auto_now_add=True)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    quantity = models.PositiveIntegerField()
+    date = models.DateTimeField(auto_now_add=True)
                 
