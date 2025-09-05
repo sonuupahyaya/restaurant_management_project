86429b794a7f7c22ea890632ed7ee734be733947
sonuupahyaya <98040472+sonuupahyaya@users.noreply.github.com>
2025-09-05 23:38:38 +0530

    42604

--- products/models.py
+++ products/models.py
@@ -4,4 +4,12 @@ class Product(models.Model):
     name = models.CharField(max_length=100)
         price = models.DecimalField(max_digits=8, decimal_places=2)
             available = models.BooleanField(default=True)
-            
+            class Restaurant(models.Model):
+                    name = models.CharField(max_length=100)
+                        address = models.TextField()
+                            phone_number = models.CharField(max_length=15, blank=True, null=True)  # NEW field
+                                logo = models.ImageField(upload_to="logos/", blank=True, null=True)
+
+                                    def __str__(self):
+                                            return self.name
+                                            
