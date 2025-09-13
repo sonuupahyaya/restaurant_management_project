85a19f607faef294f0abe147b4b90cee575d9ae8
sonuupahyaya <98040472+sonuupahyaya@users.noreply.github.com>
2025-09-13 18:34:49 +0530

    43810

--- home/urls.py
+++ home/urls.py
@@ -29,4 +29,13 @@ urlpatterns = [
                  path("about/", views.about_page, name="about"),
                      path("faq/", views.faq_page, name="faq"),  # 👈 New FAQ page
                      ]
-                     
+                     from django.urls import path
+                     from django.contrib.auth import views as auth_views
+                     from . import views
+
+                     urlpatterns = [
+                         path('', views.home, name='home'),
+                             path('login/', auth_views.LoginView.as_view(template_name='restaurant/home.html'), name='login'),
+                                 path('logout/', auth_views.LogoutView.as_view(next_page='home'), name='logout'),
+                                 ]
+                                 
