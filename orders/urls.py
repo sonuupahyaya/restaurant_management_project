00b0f7d3364b4df3862b28bfdbeb3cc8fd6419e7
sonuupahyaya <98040472+sonuupahyaya@users.noreply.github.com>
2025-10-09 15:18:50 +0530

    54228

--- orders/urls.py
+++ orders/urls.py
@@ -4,4 +4,10 @@ from . import views
 urlpatterns = [
     path('orders/', views.order_list, name='order_list'),
     ]
-    
+
+
+    menu_search = MenuItemSearchViewSet.as_view({'get': 'list'})
+
+    urlpatterns = [
+        path('menu/search/', menu_search, name='menu-search'),
+        ]
