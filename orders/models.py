cf847345cc3a623acdd03c33c14bd15e6d6ec784
sonuupahyaya <98040472+sonuupahyaya@users.noreply.github.com>
2025-10-09 15:18:41 +0530

    54228

--- orders/models.py
+++ orders/models.py
@@ -54,3 +54,12 @@ class MenuItem(models.Model):
 
                     def __str__(self):
                             return self.name
+
+class MenuItem(models.Model):
+    name = models.CharField(max_length=100)
+        description = models.TextField(blank=True)
+            price = models.DecimalField(max_digits=8, decimal_places=2)
+                is_available = models.BooleanField(default=True)
+
+                    def __str__(self):
+                            return self.name
