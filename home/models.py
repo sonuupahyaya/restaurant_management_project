500343c585db36f561d5a5ab27fd03c7ee261e58
sonu upadhyay <98040472+sonuupahyaya@users.noreply.github.com>
2025-10-11 22:13:36 +0530

    54396

--- home/models.py
+++ home/models.py
@@ -2,7 +2,16 @@ from django.db import models
 
 # Create your models here.
 from django.db import models
+from django.db import models
 
+class Table(models.Model):
+    table_number = models.IntegerField(unique=True)
+        seating_capacity = models.IntegerField()
+            status = models.CharField(max_length=20, default="Available")  # e.g., Available / Reserved
+
+                def __str__(self):
+                        return f"Table {self.table_number}"
+                        
 class MenuCategory(models.Model):
     name = models.CharField(max_length=100, unique=True)
 
