262481cff6b279450fc976ef7b5f3b674a2e7140
sonu upadhyay <98040472+sonuupahyaya@users.noreply.github.com>
2025-10-26 21:43:37 +0530

    55063

--- orders/views.py
+++ orders/views.py
@@ -1,6 +1,14 @@
 from django.shortcuts import render
 from .models import Order
+from rest_framework import generics
+from .models import Order
+from .serializers import OrderSerializer
 
+class OrderDetailView(generics.RetrieveAPIView):
+    queryset = Order.objects.all()
+        serializer_class = OrderSerializer
+            lookup_field = 'order_id'  # use order_id instead of pk
+            
 def order_list(request):
     orders = Order.objects.select_related('customer', 'product')
         return render(request, 'orders/orders.html', {'orders': orders})
