a2b0c5b8e2903d8a4571211bc5ef4679159793fc
sonu upadhyay <98040472+sonuupahyaya@users.noreply.github.com>
2025-10-26 21:50:23 +0530

    55060

--- account/settings.py
+++ account/settings.py
@@ -7,4 +7,11 @@ REST_FRAMEWORK = {}
                                     'rest_framework.permissions.IsAuthenticated',
                                         ],
                                         }
+                                        EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+                                        EMAIL_HOST = 'smtp.gmail.com'
+                                        EMAIL_PORT = 587
+                                        EMAIL_USE_TLS = True
+                                        EMAIL_HOST_USER = 'your_email@gmail.com'
+                                        EMAIL_HOST_PASSWORD = 'your_app_password'  # Use App Password if Gmail
+                                        DEFAULT_FROM_EMAIL = 'Restaurant <your_email@gmail.com>'
                                         
