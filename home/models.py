fd997ce1d5f793c6e44fd9c0e3a6347576a4f045
sonu upadhyay <98040472+sonuupahyaya@users.noreply.github.com>
2025-10-26 21:51:57 +0530

    55059

--- home/models.py
+++ home/models.py
@@ -3,7 +3,16 @@ from django.db import models
 # Create your models here.
 from django.db import models
 from django.db import models
+from django.db import models
 
+class Table(models.Model):
+    table_number = models.IntegerField(unique=True)
+        capacity = models.IntegerField()
+            is_available = models.BooleanField(default=True)
+
+                def __str__(self):
+                        return f"Table {self.table_number} (Capacity: {self.capacity})"
+                        
 class Table(models.Model):
     table_number = models.IntegerField(unique=True)
         seating_capacity = models.IntegerField()
